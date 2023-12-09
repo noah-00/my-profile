7cd36910c28a07fe481d83ef7dc24bd6adaea1e7
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-09 18:11:19 +0900

    ✨ feat: contact content

--- components/templates/Contact/index.tsx
+++ components/templates/Contact/index.tsx
@@ -5,13 +5,16 @@ import Image from 'next/image'
 import { MotionFadeInFromBottomWithScroll } from '@/components/motion/MotionFadeInFromBottomWithScroll'
 import { SectionTitle } from '@/components/parts/SectionTitle'
 
+import { useTranslation } from '@/app/i18n/index'
 import myPicture from '@/public/images/me.png'
 
 type Props = {
   lng: string
 }
 
-export const Contact = (props: Props) => {
+export const Contact = async (props: Props) => {
+  const { t } = await useTranslation(props.lng, 'main')
+
   return (
     <>
       <MotionFadeInFromBottomWithScroll>
@@ -19,14 +22,16 @@ export const Contact = (props: Props) => {
       </MotionFadeInFromBottomWithScroll>
       <MotionFadeInFromBottomWithScroll>
         <div className="hero">
-          <div className="hero-content flex-col lg:flex-row">
+          <div className="hero-content flex-col lg:flex-row space-x-8 space-y-8">
             <Image width={400} height={400} alt="me" src={myPicture} className="rounded-lg" />
-            <div>
-              <h1 className="text-5xl font-bold">Contact me</h1>
-              <p className="py-6">
-                test test test test test test test test test test test test test
+            <div className="space-y-10">
+              <h1 className="text-5xl font-bold">{t('contact.title')}</h1>
+              <p>
+                {t('contact.content')}
+                <br />
+                {t('contact.content-2')}
               </p>
-              <button className="btn btn-primary btn-outline">Say hello</button>
+              <button className="btn btn-primary btn-outline">{t('contact.button')}</button>
             </div>
           </div>
         </div>
