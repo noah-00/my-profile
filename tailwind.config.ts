071ed72f1d3884f64c285a23aa23901de0c4ea21
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-01-08 15:56:54 +0900

    💄 md: imported icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,16 @@ const config: Config = {
   plugins: [
     require('daisyui'),
     iconsPlugin({
-      collections: getIconCollections(['ion', 'nonicons', 'fa6-brands', 'clarity', 'akar-icons'])
+      collections: getIconCollections([
+        'ion',
+        'nonicons',
+        'fa6-brands',
+        'clarity',
+        'akar-icons',
+        'vscode-icons',
+        'devicon',
+        'logos'
+      ])
     })
   ],
   daisyui: {
