c02ac2d096d679d8e58b9d15699c5a859abfb271
noah-00 <t71.ishizaka.naoya@gmail.com>
2025-04-19 11:19:59 -0700

    ✨ update: remove unused state and simplify accordion component

--- components/parts/Accordion.ts/index.tsx
+++ components/parts/Accordion.ts/index.tsx
@@ -1,7 +1,3 @@
-'use client'
-
-import { useState } from 'react'
-
 import { TechBadge } from '@/components/parts/TechBadge'
 
 import { TECH_STACKS } from '@/utils/MtData'
@@ -16,25 +12,21 @@ type Props = {
 }
 
 export const Accordion = (props: Props) => {
-  const [active, setActive] = useState(false)
-
   return (
     <div className="collapse collapse-arrow border-x-[3px] border-primary my-6 hover:opacity-70 bg-base-300">
       <input
         type="checkbox"
+        className="peer"
         name={`accordion-${props.organizationKey}-${props.index}`}
-        checked={active}
-        onChange={() => setActive(!active)}
-        aria-label="Accordion"
       />
       <div className="collapse-title text-xl font-medium text-secondary">{props.title}</div>
       <div className="collapse-content">
         <div>({props.year})</div>
         <div className="my-6">{props.description}</div>
         <div>
-          {props.techStackKeys.split(',').map((key) => {
-            return <TechBadge name={TECH_STACKS[key].name} key={TECH_STACKS[key].name} />
-          })}
+          {props.techStackKeys.split(',').map((key) => (
+            <TechBadge name={TECH_STACKS[key].name} key={TECH_STACKS[key].name} />
+          ))}
         </div>
       </div>
     </div>
